2fb848f894fab08abd994155baed1731c73673cd
Khaled Adad <khaledadad@lewisu.edu>
2019-12-02 12:51:32 -0600

    added hashes

--- hashes.py
+++ hashes.py
@@ -5,3 +5,13 @@ import bcrypt
 key = b"hello"
 
 print(hashlib.sha256(key).hexdigest())
+
+
+def djb2(key):
+    # start from an arbitrarily large prime number
+    hash_value = 5381
+
+    # bit-shift and sum value for each character
+
+    for char in key:
+        hash_value = ((hash_value << 5) + hash_value) + char
